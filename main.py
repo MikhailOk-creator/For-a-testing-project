03870caf34670b74e0c6c7e8e667a2cd578131da
Mikhail OKhapkin <mikhail.okhapkin@yandex.ru>
2022-10-10 14:05:02 +0300

    Сделал свою часть

--- main.py
+++ main.py
@@ -31,6 +31,28 @@ def transpose(mas):
     return trans_mas
 
 
+def discharged_matrix(mas):
+    for i in range(len(list(mas))):
+        for j in range(len(mas)):
+            if i == j:
+                mas[i][j] = 0
+
+    # print(mas)
+    return mas
+
+
+def is_discharged_matrix(mas):
+    counter = 0
+    for i in range(len(list(mas))):
+        for j in range(len(mas)):
+            if mas[i][j] == 0:
+                counter = counter + 1
+
+    m = len(list(mas))
+    n = len(mas)
+    return (counter >
+            ((m * n) // 2))
+
 # determ(mas)
 # minus(mas)
 
--- test.py
+++ test.py
@@ -19,5 +19,16 @@ class MyTestCase(unittest.TestCase):
     def test_transpose(self):
         self.assertEqual(transpose([[2, 1, 3], [3, 1, 5]]), [[2, 3], [1, 1], [3, 5]])
 
+    class Test(unittest.TestCase):
+        def test_discharged_matrix(self):
+            self.assertEqual(discharged_matrix([[1, 2], [3, 4]]), [[0, 2], [3, 0]])
+
+        def test_is_discharged_matrix(self):
+            self.assertEqual(is_discharged_matrix([[0, 0], [3, 0]]), True)
+
+        def test_is_discharged_matrix1(self):
+            self.assertEqual(is_discharged_matrix([[1, 2], [3, 4]]), False)
+
+
 if __name__ == '__main__':
     unittest.main()
